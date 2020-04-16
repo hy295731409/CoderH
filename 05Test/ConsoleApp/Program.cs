c289303b6a22a2df236d24cfa8f3ef88e796872b
huangxiaoping <huangxiaoping@medicom.com.cn>
2020-04-16 17:23:25 +0800

    del

--- 05Test/ConsoleApp/Program.cs
+++ 05Test/ConsoleApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using AccessWinform;
-using RabbitMQClient;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,37 +12,7 @@ namespace JSHC.RabbitMQClient.ConsoleHost
     {
         static void Main(string[] args)
         {
-            LoadDLLTest.ReadDll();
-            //Send();
-            //Console.Read();
-        }
-
-        private static void Send()
-        {
-            for (int i = 0; i < 5; i++)
-            {
-                //Thread.Sleep(400);
-                string message = $" message{i}";
-                RabbitMqClient.Instance.TriggerEventMessage(message, "MQ.Advice.Queue", "MQ.Advice.Queue");
-                Console.WriteLine($"send : {message}");
-                Thread.Sleep(100);
-            }
-
-            Receive();
-        }
-
-        private static void Receive()
-        {
-            RabbitMqClient.Instance.ActionEventMessage += mqClient_ActionEventMessage;
-            RabbitMqClient.Instance.ListenInit("MQ.Advice.Queue", "MQ.Advice.Queue");
-        }
-
-        private static void mqClient_ActionEventMessage(EventMessageResult result)
-        {
-
-            result.IsOperationOk = true; //处理成功
-
-            Console.WriteLine(result.MessageBody);
+            
         }
         
     }
