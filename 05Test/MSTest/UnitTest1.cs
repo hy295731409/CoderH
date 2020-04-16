b7c58135abaab076b9177110714d2e40e13f33e6
huangxiaoping <huangxiaoping@medicom.com.cn>
2020-04-16 17:46:00 +0800

    add .

--- 05Test/MSTest/UnitTest1.cs
+++ 05Test/MSTest/UnitTest1.cs
@@ -1,5 +1,6 @@
 using CoreWebApi4Docker.Controllers;
-using Domain.Entitys;
+using Domain.Interface;
+using Entity;
 using Framework.DB.Infrastructure;
 using Framework.DB.Utility.Helper;
 using Microsoft.Extensions.Logging;
@@ -14,11 +15,12 @@ namespace MSTest
         WeatherForecastController controller;
         Mock<IRepository<WeatherForecast>> moqRep;
         Mock<ILogger<WeatherForecastController>> logger;
+        Mock<IWeatherForecastService> service;
         public UnitTest1()
         {
             logger = new Mock<ILogger<WeatherForecastController>>();
             moqRep = new Mock<IRepository<WeatherForecast>>();
-            controller = new WeatherForecastController(logger.Object);
+            controller = new WeatherForecastController(logger.Object, service.Object);
         }
 
         [TestMethod]
@@ -31,7 +33,7 @@ namespace MSTest
         [TestMethod]
         public void ControllerTest()
         {
-            var res = controller.Get();
+            var res = controller.Get(null);
             logger.Object.LogError("dddddd");
             var res2 = moqRep.Object.Get(1);
             Assert.IsNotNull(res);
